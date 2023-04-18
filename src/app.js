111be221f8f9025295e45eb2358df0a04e74d0b9
JEsteban2D <juanestebancossio21@gmail.com>
2023-04-18 18:46:18 -0500

    fix error on cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 
-import express from "express"
+import {express, cors} from "express"
 import usersRoutes from './routes/users.routes.js'
 import indexRoutes from './routes/index.routes.js'
-const express = require('express')
-const cors = require('cors')
 
 const app = express()
 

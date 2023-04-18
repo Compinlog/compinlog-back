c183112f7e28cc2fc231b35cbacfa404cb37c32e
JEsteban2D <juanestebancossio21@gmail.com>
2023-04-18 18:19:52 -0500

    fix app cors

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@
 import express from "express"
 import usersRoutes from './routes/users.routes.js'
 import indexRoutes from './routes/index.routes.js'
-import {PORT} from './config.js'
 
-const app = express()
 const cors = require('cors')
 
-app.use(express.json())
+const app = express()
+
+
 app.use(cors())
+
+app.use(express.json())
+
 
 app.use(indexRoutes)
 app.use('/api', usersRoutes)

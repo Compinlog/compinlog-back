64e6b95d28fe1e1f12c7c012d924fa468f627c56
JEsteban2D <juanestebancossio21@gmail.com>
2023-04-18 18:41:36 -0500

    run cors on local

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@
 import express from "express"
 import usersRoutes from './routes/users.routes.js'
 import indexRoutes from './routes/index.routes.js'
+const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(indexRoutes)
 app.use('/api', usersRoutes)
